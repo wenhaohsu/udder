5940a551138ad50283a2ac18992e6a05960816e2
Hao <gary2582003@gmail.com>
2021-05-20 12:07:09 +0800

    Initial Commit

---
+++ udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  udder
+//
+//  Created by 許文豪 on 2021/5/20.
+//  Copyright © 2021 許文豪. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
